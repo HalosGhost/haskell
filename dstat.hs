2bc2999cfb934c9744baac375e2850506f9251e9
HalosGhost <halosghost@archlinux.info>
2015-06-07 10:21:01 -0500

    Yay, it's functional!
    use if-then-else instead of that manual ternary conditional operator
    use explicit imports
    add X11 integration
    keep lines <80 chars
    recurse for update interval

--- dstat.hs
+++ dstat.hs
@@ -1,5 +1,12 @@
-import System.Process
-import Data.Time
+module Main where
+
+import System.Process (readProcess)
+import Data.Time (getZonedTime, formatTime, defaultTimeLocale)
+import Control.Concurrent (threadDelay)
+import Graphics.X11.Xlib.Event (sync)
+import Graphics.X11.Xlib.Display (defaultRootWindow, openDisplay, closeDisplay)
+import Graphics.X11.Xlib.Types (Display)
+import Graphics.X11.Xlib.Window (storeName)
 
 bat_loc = "/sys/class/power_supply/"
 en_loc  = "/sys/class/net/"
@@ -9,36 +16,28 @@ bat_level :: String -> IO Int
 bat_level b = do cap <- readFile $ bat_loc ++ b ++ "/capacity"
                  return . read $ take 2 cap
 
--- My very own ternary operator!
-infix 1 ?
-(?) :: Bool -> a -> a -> a
-(?) True  x _ = x
-(?) False _ y = y
-
--- eventually replace with an ACPI module
 bat_status :: String -> IO Char
 bat_status b = do stat <- readFile $ bat_loc ++ b ++ "/status"
-                  return $ head stat == 'C' ? 'ϟ' $ 'D'
+                  return $ if head stat == 'C' then 'ϟ' else 'D'
 
 bat_comp :: String -> IO String
 bat_comp b = do l <- bat_level b
                 s <- bat_status b
                 return $ show l ++ [s]
 
--- eventually replace with a pulseaudio module
 vol :: IO Int
 vol = do v <- readProcess "ponymix" ["get-volume"] []
          return $ read $ init v
 
 en_stat :: String -> IO String
 en_stat e = do stat <- readFile $ en_loc ++ e ++ "/operstate"
-               return $ head stat == 'u' ? "U" $ "D"
+               return $ if head stat == 'u' then "U" else "D"
 
 wl_str :: String -> IO Int
 wl_str s = do stat <- readFile $ wl_loc
               let status_line = words . last . lines $ stat
-              let connected   = (init . head $ status_line) == s
-              return $ connected ? (read $ take 2 $ status_line!!2) $ -1
+              let cnctd       = (init . head $ status_line) == s
+              return $ if cnctd then (read $ take 2 $ status_line!!2) else -1
 
 wl_bars :: Int -> String
 wl_bars s | s < 0     = "No Signal" | s <= 10 = "▂"
@@ -50,9 +49,25 @@ wl_bars s | s < 0     = "No Signal" | s <= 10 = "▂"
 cur_time :: String -> IO String
 cur_time f = getZonedTime >>= return . formatTime defaultTimeLocale f
 
+stats ::IO String
+stats = do b <- bat_comp "BAT0"
+           vl <- vol
+           let v = show vl
+           e <- en_stat "enp0s25"
+           wl <- wl_str "wlp3s0"
+           let w = wl_bars wl
+           t <- cur_time "%H.%M (%Z) | %A, %d %B %Y"
+           return $ "E: "++e++" | W: "++w++" | A: "++v++" | B: "++b++" | "++t
+
+status :: Display -> IO ()
+status d = do let w = defaultRootWindow d
+              s <- stats
+              storeName d w s
+              sync d False
+              threadDelay 6000000
+              status d
+
 main :: IO ()
---main = vol >>= putStrLn . show
---main = bat_comp "BAT0" >>= putStrLn
---main = en_stat "enp0s25" >>= putStrLn
---main = wl_str "wlp3s0" >>= putStrLn . wl_bars
-main = cur_time "%H.%M (%Z) | %A, %d %B %Y" >>= putStrLn
+main = do d <- openDisplay ""
+          status d
+          closeDisplay d
