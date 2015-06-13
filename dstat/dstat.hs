d3e9cc33842bdea7ca059c7d6ff3ad3a59ed5223
HalosGhost <halosghost@archlinux.info>
2015-06-13 11:28:46 -0500

    camelCase (ugh)

--- dstat/dstat.hs
+++ dstat/dstat.hs
@@ -13,22 +13,22 @@ import Graphics.X11.Xlib.Window (storeName)
 import System.Exit (exitWith, ExitCode(ExitSuccess, ExitFailure))
 import System.Environment (getArgs)
 
-bat_loc = "/sys/class/power_supply/"
-en_loc  = "/sys/class/net/"
-wl_loc  = "/proc/net/wireless"
+batLoc = "/sys/class/power_supply/"
+enLoc  = "/sys/class/net/"
+wlLoc  = "/proc/net/wireless"
 
-bat_level :: String -> IO Int
-bat_level b = do cap <- readFile $ bat_loc ++ b ++ "/capacity"
-                 return . read $ take 2 cap
+batLevel :: String -> IO Int
+batLevel b = do cap <- readFile $ batLoc ++ b ++ "/capacity"
+                return . read $ take 2 cap
 
-bat_status :: String -> IO Char
-bat_status b = do stat <- readFile $ bat_loc ++ b ++ "/status"
-                  return $ if head stat == 'C' then 'ϟ' else 'D'
+batStatus :: String -> IO Char
+batStatus b = do stat <- readFile $ batLoc ++ b ++ "/status"
+                 return $ if head stat == 'C' then 'ϟ' else 'D'
 
-bat_comp :: String -> IO String
-bat_comp b = do l <- bat_level b
-                s <- bat_status b
-                return $ "B: " ++ show l ++ [s]
+batComp :: String -> IO String
+batComp b = do l <- batLevel b
+               s <- batStatus b
+               return $ "B: " ++ show l ++ [s]
 
 vol :: IO Int
 vol = do v <- readProcess "ponymix" ["get-volume"] []
@@ -39,29 +39,29 @@ muted = do p <- runCommand "ponymix is-muted"
            e <- waitForProcess p
            return $ if e /= (exitCode 0) then '%' else 'X'
 
-vol_comp :: IO String
-vol_comp = do v <- vol
-              m <- muted
-              return $ "A: " ++ show v ++ [m]
+volComp :: IO String
+volComp = do v <- vol
+             m <- muted
+             return $ "A: " ++ show v ++ [m]
 
-en_stat :: String -> IO String
-en_stat e = do stat <- readFile $ en_loc ++ e ++ "/operstate"
-               return $ "E: " ++ (if head stat == 'u' then "U" else "D")
+enStatus :: String -> IO String
+enStatus e = do stat <- readFile $ enLoc ++ e ++ "/operstate"
+                return $ "E: " ++ (if head stat == 'u' then "U" else "D")
 
-wl_str :: String -> IO Int
-wl_str s = do stat <- readFile $ wl_loc
-              let status_line = words . last . lines $ stat
-              let cnctd       = (init . head $ status_line) == s
-              return $ if cnctd then (read $ take 2 $ status_line!!2) else -1
+wlStr :: String -> IO Int
+wlStr s = do stat <- readFile $ wlLoc
+             let status_line = words . last . lines $ stat
+             let cnctd       = (init . head $ status_line) == s
+             return $ if cnctd then (read $ take 2 $ status_line!!2) else -1
 
-wl_bars :: Int -> String
-wl_bars s | s > 60 = "▂▃▄▅▆▇█" | s > 50    = "▂▃▄▅▆▇"
-          | s > 40 = "▂▃▄▅▆"   | s > 30    = "▂▃▄▅"
-          | s > 20 = "▂▃▄"     | s > 10    = "▂▃"
-          | s >= 0 = "▂"       | otherwise = "No Signal"
+wlBars :: Int -> String
+wlBars s | s > 60 = "▂▃▄▅▆▇█" | s > 50    = "▂▃▄▅▆▇"
+         | s > 40 = "▂▃▄▅▆"   | s > 30    = "▂▃▄▅"
+         | s > 20 = "▂▃▄"     | s > 10    = "▂▃"
+         | s >= 0 = "▂"       | otherwise = "No Signal"
 
-cur_time :: String -> IO String
-cur_time f = getZonedTime >>= return . formatTime defaultTimeLocale f
+curTime :: String -> IO String
+curTime f = getZonedTime >>= return . formatTime defaultTimeLocale f
 
 (<|>) :: String -> String -> String
 f <|> s = f ++ " | " ++ s
@@ -69,12 +69,12 @@ f <|> s = f ++ " | " ++ s
 type StatOpts = (String, String, String, String)
 
 stats :: StatOpts -> IO String
-stats (bt, en, wl, cl) = do b <- bat_comp bt
-                            v <- vol_comp
-                            e <- en_stat en
-                            w <- wl_str wl
-                            let wc = "W: " ++ wl_bars w
-                            t <- cur_time cl
+stats (bt, en, wl, cl) = do b <- batComp bt
+                            v <- volComp
+                            e <- enStatus en
+                            w <- wlStr wl
+                            let wc = "W: " ++ wlBars w
+                            t <- curTime cl
                             return $ e <|> wc <|> v <|> b <|> t
 
 status :: Maybe Display -> StatOpts -> IO ()
