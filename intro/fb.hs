766030ff3ba0f17a9755c8498f51adbe82b376f1
HalosGhost <halosghost@archlinux.info>
2015-06-15 20:33:44 -0500

    Use library functions and keep scope clean

--- intro/fb.hs
+++ intro/fb.hs
@@ -1,9 +1,10 @@
+import Data.List (intercalate)
+
 fb :: Integer -> String
 fb n | n `rem` 15 == 0 = "FizzBuzz"
      | n `rem` 5  == 0 = "Buzz"
      | n `rem` 3  == 0 = "Fizz"
      | otherwise       = show n
 
-fzbz = [ fb x | x <- [1..]]
-
-main = putStrLn . concat $ map (++ " ") $ take 100 fzbz
+main = putStrLn . intercalate " " $ take 100 fzbz
+     where fzbz = [fb x | x <- [1..]]
