d82f4023c83ba8e31c5b1ffa35f0d4798502274e
halosghost <halosghost@archlinux.info>
2015-07-23 18:43:04 -0500

    Add the first (incomplete) run of sharped-radix integer literals

---
+++ numbers/radix.hs
@@ -0,0 +1,38 @@
+module Radix ( Radix          , RadixEncodedInt
+             , fromRadixString, (♯)
+             , toRadixString  , (♭)
+             ) where
+
+import Data.List
+import Data.Maybe
+
+type Radix           = Integer
+type RadixEncodedInt = String
+
+fromRadixString :: Radix -> RadixEncodedInt -> Integer
+fromRadixString r s | r < 1 || r > 36  = error rangeErrMsg
+                    | invalidStr       = error $ s ++ glyphErrMsg ++ show r
+                    | otherwise        = sum . valuesPerPlace r 0 $ reverse s
+                    where glyphErrMsg = " is not a valid number in radix "
+                          rangeErrMsg = "Radix must be in the range [1..36]"
+                          cs          = genericTake r chars
+                          chars       = '1' : '0' : ['2' .. '9'] ++ ['a' .. 'z']
+                          invalidStr  = elem False $ map (`elem` cs) s
+
+valuesPerPlace :: Radix -> Integer -> RadixEncodedInt -> [Integer]
+valuesPerPlace r p []     = []
+valuesPerPlace r p (c:cs) | cs == []  = [n]
+                          | otherwise =  n : valuesPerPlace r (p + 1) cs
+                          where n    = x * r ^ p
+                                x    = toInteger . fromJust . elemIndex c $ vals
+                                vals = ['0' .. '9'] ++ ['a' .. 'z']
+
+(♯) :: Radix -> RadixEncodedInt -> Integer
+r♯s = fromRadixString r s
+
+toRadixString :: Radix -> Integer -> RadixEncodedInt
+toRadixString r i | r < 1 || r > 36 = error "Radix must be in the range [1..36]"
+                  | otherwise       = "1" -- Finish implementing
+
+(♭) :: Radix -> Integer -> RadixEncodedInt
+r♭i = toRadixString r i
