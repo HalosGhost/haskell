18d809ae91390e529385b5ad7243f26457410304
halosghost <halosghost@archlinux.info>
2015-08-06 21:55:05 -0500

    Genericize for Integral rather than Int or Integer

--- numbers/radix.hs
+++ numbers/radix.hs
@@ -1,4 +1,4 @@
-module Radix ( Radix          , RadixEncodedInt
+module Radix ( RadixEncodedInt
              , fromRadixString, (♯)
              , toRadixString  , (♭)
              ) where
@@ -6,14 +6,19 @@ module Radix ( Radix          , RadixEncodedInt
 import Data.List
 import Data.Maybe
 
-type Radix           = Integer
 type RadixEncodedInt = String
 
-fromRadixChar :: Char -> Integer
-fromRadixChar c = toInteger . fromJust $ elemIndex c vals
+fromRadixChar :: Integral a => Char -> a
+fromRadixChar c = fromIntegral . fromJust $ elemIndex c vals
                 where vals = ['0'..'9'] ++ ['a'..'z']
 
-fromRadixString :: Radix -> RadixEncodedInt -> Integer
+valuesPerPlace :: Integral a => a -> Int -> RadixEncodedInt -> [a]
+valuesPerPlace r p []     = []
+valuesPerPlace r p (c:cs) | cs == []  = [n]
+                          | otherwise =  n : valuesPerPlace r (p + 1) cs
+                          where n = (fromRadixChar c) * r ^ p
+
+fromRadixString, (♯) :: (Show a, Integral a) => a -> RadixEncodedInt -> a
 fromRadixString r s | r < 1 || r > 36 = error rangeErrMsg
                     | invalidStr      = error $ glyphErrMsg ++ show r
                     | r == 1          = genericLength s
@@ -24,25 +29,17 @@ fromRadixString r s | r < 1 || r > 36 = error rangeErrMsg
                           chars       = '1' : '0' : ['2'..'9'] ++ ['a'..'z']
                           invalidStr  = elem False $ map (`elem` cs) s
 
-valuesPerPlace :: Radix -> Integer -> RadixEncodedInt -> [Integer]
-valuesPerPlace r p []     = []
-valuesPerPlace r p (c:cs) | cs == []  = [n]
-                          | otherwise =  n : valuesPerPlace r (p + 1) cs
-                          where n = (fromRadixChar c) * r ^ p
-
-(♯) :: Radix -> RadixEncodedInt -> Integer
 r♯s = fromRadixString r s
 
-toRadixChar :: Integer -> Char
-toRadixChar i = vals !! fromInteger i
+toRadixChar :: Integral a => a -> Char
+toRadixChar i = vals !! fromIntegral i
               where vals = ['0' .. '9'] ++ ['a' .. 'z']
 
-toRadixString :: Radix -> Integer -> RadixEncodedInt
+toRadixString, (♭) :: Integral a => a -> a -> RadixEncodedInt
 toRadixString r i | r < 1 || r > 36 = error "Radix must be in the range [1..36]"
                   | r == 1          = genericReplicate i '1'
                   | otherwise       = reverse [toRadixChar x | x <- as i r]
                   where as 0 _ = []
                         as n b = n `rem` b : as (n `div` b) b
 
-(♭) :: Radix -> Integer -> RadixEncodedInt
 r♭i = toRadixString r i
