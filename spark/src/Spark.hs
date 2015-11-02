f47e1ccb4e31e5502ae635b7aac92782d76e2504
halosghost <halosghost@archlinux.info>
2015-11-02 16:58:14 -0600

    Remove redundant Foldable

--- spark/src/Spark.hs
+++ spark/src/Spark.hs
@@ -5,9 +5,9 @@ import qualified Data.Foldable as F
 sparkChar :: RealFrac f => f -> Char
 sparkChar n = "▂▃▄▅▆▇█" !! (round $ n * 6)
 
-normalize :: (Traversable t, Foldable t, Ord a, RealFrac a) => t a -> t a
+normalize :: (Traversable t, Ord a, RealFrac a) => t a -> t a
 normalize is = (/n) <$> is
         where n = F.maximum is
 
-sparkLine :: (Traversable t, Foldable t, Ord a, RealFrac a) => t a -> t Char
+sparkLine :: (Traversable t, Ord a, RealFrac a) => t a -> t Char
 sparkLine is = sparkChar <$> (normalize is)
