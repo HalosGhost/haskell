27f02ba6458db48294255065cfb6962fba1379cc
halosghost <halosghost@archlinux.info>
2016-05-14 12:00:03 -0500

    Fill out initial data type and conversion from char to instruction

--- bf/BF.hs
+++ bf/BF.hs
@@ -1,1 +1,35 @@
 module Brainfuck where
+
+data BFInstruction
+  = BFIgnored
+  -- | BFPragma BFPragmaValue
+  | BFIncrement
+  | BFDecrement
+  | BFRightShift
+  | BFLeftShift
+  | BFPutChar
+  | BFGetChar
+  | BFLoopStart
+  | BFLoopEnd
+  deriving (Eq)
+
+{-
+data BFPragmaValue
+  = BFToggleDebug
+  | BFEOFBehavior Int
+  | BF
+-}
+
+toBFInst :: Char -> BFInstruction
+toBFInst c
+  = case c of
+      '+' -> BFIncrement
+      '-' -> BFDecrement
+      '>' -> BFRightShift
+      '<' -> BFLeftShift
+      '.' -> BFPutChar
+      ',' -> BFGetChar
+      '[' -> BFLoopStart
+      ']' -> BFLoopEnd
+      _   -> BFIgnored
+
