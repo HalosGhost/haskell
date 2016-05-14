3f9e69da11a6fff0cbbfefbb1706c130cbefbd04
halosghost <halosghost@archlinux.info>
2016-05-14 13:33:32 -0500

    Fill out the first part of executeBF

--- bf/BF.hs
+++ bf/BF.hs
@@ -33,3 +33,22 @@ toBFInst c
       ']' -> BFLoopEnd
       _   -> BFIgnored
 
+type BFState = (Int, [Int])
+
+executeBF :: [BFInstruction] -> BFState -> IO BFState
+executeBF []     s       = return s
+executeBF (i:is) s@(p,t) = case i of
+    BFRightShift -> return (p + 1, t)
+    BFLeftShift  -> return (p - 1, t)
+    BFIncrement  -> return (p, incr)
+    BFDecrement  -> return (p, decr)
+    BFPutChar    -> putChar (toEnum $ t !! p) >> return s
+    BFGetChar    -> do c <- getChar; return (p, chard c)
+    BFLoopStart  -> executeBF is s
+    _            -> return s
+  where (b, e)  = splitAt p t
+        cadre   = head e
+        cabse   = tail e
+        incr    = concat [b, [cadre + 1], cabse]
+        decr    = concat [b, [cadre - 1], cabse]
+        chard c = concat [b, [fromEnum c  ], cabse]
