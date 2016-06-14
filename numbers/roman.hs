fb13364133efde9e2dc4a001c1161fecdb1dfdee
halosghost <halosghost@archlinux.info>
2016-06-14 18:01:53 -0500

    refactor additiveToSubtractive and subtractiveToAdditive

--- numbers/roman.hs
+++ numbers/roman.hs
@@ -58,15 +58,17 @@ fromAdditiveRoman' (n:ns) = fromRomanGlyph [n] + (fromAdditiveRoman' ns)
 fromSubtractiveRoman' :: Integral a => String -> a
 fromSubtractiveRoman' = fromAdditiveRoman' . subtractiveToAdditive
 
+substitutionMap = zip ["IIII", "VIIII", "XXXX", "LXXXX", "CCCC", "DCCCC"]
+                      ["IV",   "IX",    "XL",   "XC",    "CD",   "CM"]
+
+subStep :: (String, String) -> String -> String
+subStep (f, s) = replace f s
+
 additiveToSubtractive :: String -> String
-additiveToSubtractive = replace "IIII"  "IV" . replace "VIIII" "IX"
-                      . replace "XXXX"  "XL" . replace "LXXXX" "XC"
-                      . replace "CCCC"  "CD" . replace "DCCCC" "CM"
+additiveToSubtractive s = foldr subStep s substitutionMap
 
 subtractiveToAdditive :: String -> String
-subtractiveToAdditive = replace "IV" "IIII"  . replace "IX" "VIIII"
-                      . replace "XL" "XXXX"  . replace "XC" "LXXXX"
-                      . replace "CD" "CCCC"  . replace "CM" "DCCCC"
+subtractiveToAdditive s = foldr subStep s $ map swap substitutionMap
 
 valToSubtract :: Integral a => a -> [a] -> a
 valToSubtract n []     = 0
