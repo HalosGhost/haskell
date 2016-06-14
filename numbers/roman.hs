ebf8d54ad12a8d369b47a2b0b71f78ea0738a0e0
halosghost <halosghost@archlinux.info>
2016-06-14 18:53:42 -0500

    Add Lenient forms

--- numbers/roman.hs
+++ numbers/roman.hs
@@ -6,29 +6,38 @@ module Roman ( toRoman
 import Data.List (group, stripPrefix)
 import Data.Tuple (swap)
 
--- | Data type for specifying the format of the Roman Numeral
+-- | Data type for specifying the format of the Roman Numeral.
 data RomanFormat
   = Additive     -- ^ Strictly Additive (e.g., IIII, not IV) and 0 == ""
   | AdditiveN    -- ^ Strictly Additive (e.g., IIII, not IV) and 0 == "N"
   | Subtractive  -- ^ Strictly Subtractive (e.g., IV, not IIII) and 0 == ""
   | SubtractiveN -- ^ Strictly Subtractive (e.g., IV, not IIII) and 0 == "N"
+  | Lenient      -- ^ Lenient Subtractive (e.g., IM == 999) and 0 == ""
+  | LenientN     -- ^ Lenient Subtractive (e.g., IM == 999) and 0 == "N"
   deriving (Eq)
 
--- | Convert an integral to a Roman Numeral of the specified format
+-- | Convert an integral to a Roman Numeral of the specified format.
+-- Because Lenient forms do not have single representation of all numbers
+-- and because all Subtractive forms are valid Lenient forms,
+-- `toRoman Lenient{,N}` will use the Subtractive forms.
 toRoman :: Integral a => RomanFormat -> a -> String
 toRoman f n = case f of
   Additive     -> toAdditiveRoman'    n
   AdditiveN    -> toAdditiveRoman     n
   Subtractive  -> toSubtractiveRoman' n
   SubtractiveN -> toSubtractiveRoman  n
+  Lenient      -> toSubtractiveRoman' n
+  LenientN     -> toSubtractiveRoman  n
 
--- | Convert a Roman Numeral of the specified format to an integral
+-- | Convert a Roman Numeral of the specified format to an integral.
 fromRoman :: Integral a => RomanFormat -> String -> a
 fromRoman f s = case f of
   Additive     -> fromAdditiveRoman'    s
   AdditiveN    -> fromAdditiveRoman     s
   Subtractive  -> fromSubtractiveRoman' s
   SubtractiveN -> fromSubtractiveRoman  s
+  Lenient      -> fromLenientRoman'     s
+  LenientN     -> fromLenientRoman      s
 
 toAdditiveRoman :: Integral a => a -> String
 toAdditiveRoman 0 = "N"
@@ -44,6 +53,10 @@ fromAdditiveRoman n   = fromAdditiveRoman' n
 fromSubtractiveRoman :: Integral a => String -> a
 fromSubtractiveRoman = fromAdditiveRoman . subtractiveToAdditive
 
+fromLenientRoman :: Integral a => String -> a
+fromLenientRoman "N" = 0
+fromLenientRoman s   = fromLenientRoman' s
+
 toAdditiveRoman' :: Integral a => a -> String
 toAdditiveRoman' 0 = ""
 toAdditiveRoman' n = toRomanGlyph n' ++ (toAdditiveRoman' $ n - n')
@@ -52,11 +65,19 @@ toAdditiveRoman' n = toRomanGlyph n' ++ (toAdditiveRoman' $ n - n')
 toSubtractiveRoman' :: Integral a => a -> String
 toSubtractiveRoman' = additiveToSubtractive . toAdditiveRoman'
 
+fromAdditiveRoman' :: Integral a => String -> a
 fromAdditiveRoman' ""     = 0
 fromAdditiveRoman' (n:ns) = fromRomanGlyph [n] + (fromAdditiveRoman' ns)
 
 fromSubtractiveRoman' :: Integral a => String -> a
 fromSubtractiveRoman' = fromAdditiveRoman' . subtractiveToAdditive
+
+fromLenientRoman' :: Integral a => String -> a
+fromLenientRoman' ""     = 0
+fromLenientRoman' [n]    = fromRomanGlyph [n]
+fromLenientRoman' (n:ns) = (fromLenientRoman' ns) + if c >= x then c else (- c)
+  where c = fromRomanGlyph [n]
+        x = fromRomanGlyph [head ns]
 
 substitutionMap = zip ["IIII", "VIIII", "XXXX", "LXXXX", "CCCC", "DCCCC"]
                       ["IV",   "IX",    "XL",   "XC",    "CD",   "CM"]
